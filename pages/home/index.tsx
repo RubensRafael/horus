7d8806539620b2ba68ab04a1dae4220001d3969d
RubensRafael <rubensrafael2@live.com>
2022-11-07 17:21:01 -0300

    remove console.log

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,7 +2,6 @@ import { useSession } from 'next-auth/react';
 
 const Home = () => {
   const { data, status } = useSession();
-  console.log({ data, status });
   return <div>{data?.user?.name}</div>;
 };
 
